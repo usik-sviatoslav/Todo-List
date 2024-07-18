0f2652a46f18bd3c001e11df7ee9fd34ae442f83
usik-sviatoslav <usik.sviatoslav.dev@gmail.com>
2024-07-18 05:19:20 +0300

    Updated `User` model

--- todo_list/apps/user/admin.py
+++ todo_list/apps/user/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin  # noqa: F401
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ("email", "is_staff", "is_email_verified")
--- todo_list/apps/user/models.py
+++ todo_list/apps/user/models.py
@@ -1,6 +1,12 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+
+
+class CustomUserManager(UserManager):
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_email_verified", True)
+        return super().create_superuser(username, email, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -8,7 +14,10 @@ class User(AbstractUser):
     REQUIRED_FIELDS = ["username"]
 
     email = models.EmailField(_("email"), unique=True)
+    is_email_verified = models.BooleanField(default=False)
     username = models.CharField(_("username"), max_length=150)
 
+    objects = CustomUserManager()
+
     def __str__(self):
-        return self.get_full_name()
+        return self.email
